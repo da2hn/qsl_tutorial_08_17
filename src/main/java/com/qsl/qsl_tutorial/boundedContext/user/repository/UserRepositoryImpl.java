4f8d3ba09ef353c1306a855c4b9582a8fd8b6c87
apple7278 <apple7278@naver.com>
2025-09-07 16:51:11 +0900

    JPAQueryFactory bean 추가

--- src/main/java/com/qsl/qsl_tutorial/boundedContext/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/qsl/qsl_tutorial/boundedContext/user/repository/UserRepositoryImpl.java
@@ -1,4 +1,23 @@
 package com.qsl.qsl_tutorial.boundedContext.user.repository;
 
+import com.qsl.qsl_tutorial.boundedContext.user.entity.QSiteUser;
+import com.qsl.qsl_tutorial.boundedContext.user.entity.SiteUser;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepositoryCustom{
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public SiteUser getQslUser(Long id) {
+
+        QSiteUser siteUser = QSiteUser.siteUser;
+
+        return jpaQueryFactory
+                .selectFrom(siteUser) // SELECT * FROM site_user
+                .where(siteUser.id.eq(id)) //Where id = 1
+                .fetchOne(); // 단일 결과를 반환
+
+    }
 }
